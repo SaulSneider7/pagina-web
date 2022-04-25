4ddb84d9e36090a3131fbdf76c7aa192d33d4f14
JoelRavenna <ravemixjoel@gmail.com>
2022-04-24 19:41:26 -0500

    borrar vision

--- nosotros.php
+++ nosotros.php
@@ -6,8 +6,7 @@
             maxime corporis consequatur aliquid cumque doloribus ad, esse necessitatibus! Quo ducimus ipsum, vitae
             explicabo animi fugiat.</p>
         <h1>Vision</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem iusto nulla deserunt praesentium,
-            soluta quas odio, corrupti modi, nobis ipsa ut ab quasi accusamus. Dolorum libero doloremque hic ea quo.</p>
+        
     </div>
 
 </main>
