245bd65879ac602df71dcdbc5cf267632e7ce3dd
JoelRavenna <ravemixjoel@gmail.com>
2022-04-26 09:49:57 -0500

    intento fallido

--- nosotros.php
+++ nosotros.php
@@ -1,13 +1,38 @@
 <?php include('./layout/header.php') ?>
 <main>
-    <div class="container">
-        <h1>Mision</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet minus accusamus deserunt perferendis? Earum
-            maxime corporis consequatur aliquid cumque doloribus ad, esse necessitatibus! Quo ducimus ipsum, vitae
-            explicabo animi fugiat.</p>
-        <h1>Vision</h1>
-        <p>Nuestra Vision es ser la empresa mas centrada en el cliente, para contruir un lugar donde las personas puedan buscar y decubrir cualquier cosa que deseen comprar en linea.</p>
-    </div>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <style>
+      .jey{
+        width: 90%;
+        height: 400px;
+      }
+    </style>
+        <div class="container">
+            <div class="row">
+                <div class="rool">
+                    <img class="img-fluid jey" src="./img/fondoxd.jpg">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
 
 </main>
 <?php include ('./layout/footer.php') ?>
