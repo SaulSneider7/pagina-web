4356b730ce904ba276c7c2e7491fbed03925d340
JoelRavenna <ravemixjoel@gmail.com>
2022-04-26 09:48:59 -0500

    intento fallido

--- layout/header.php
+++ layout/header.php
@@ -44,7 +44,7 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="tiendas.php">Nuestras Tiendas</a>
-                        </li>
+                       </li>
                     </ul>
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <form class="d-flex">
