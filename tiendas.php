5ecd6a6a3386625495fe1bec8c3c4949634cbd6f
SaulSnedider7 <saulsneider@hotmail.com>
2022-05-01 18:30:43 -0500

    se agrego tiendas

--- tiendas.php
+++ tiendas.php
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
+<?php include('layout/header.php'); ?>
 <body>
     <style>
-    * {
-        background-color: red;
-    }
+        body{
+            background-color:#CEF6F5;
+        }
     </style>
-    <h1>
-        NUESTRAS TIENDAS
-    </h1>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <h2 class="m-5">ENCUENTRANOS EN:</h2>
+            <p>2W2Q+6GR, Isidro Bonifaz, San Martín de Porres 15311</p></br>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.6399873371774!2d-77.06341888464968!3d-11.
+                999394244289322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105ceff10e2c429%3A0x5428ad434418dcff!2sSENATI%20-%20
+                Lima%20Norte!5e0!3m2!1ses!2spe!4v1651191916418!5m2!1ses!2spe" width="600" height="450" style="border:0;" allowfullscreen=""
+                 loading="lazy" referrerpolicy="no-referrer-when-downgrade" width="1200" heigth="300" style="border:0;" allowfullscreen="" 
+                 loading="lazy" class="d-block w-100"></iframe>
+            </br>
+        </div>
+    </div>
 </body>
-
-</html>
+<?php include('layout/footer.php'); ?>
